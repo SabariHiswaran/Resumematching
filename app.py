8e0fd7b87ff6f5afff3abf16768009664b64b43a
Sabari <40663104+SabariHiswaran@users.noreply.github.com>
2025-04-19 13:17:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -139,5 +139,7 @@ def match_resume():
         jd_keywords=jd_keywords
     )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
